2dfff95b9f454c5471be99c8270ba323a36f0429
Sanduni Sathsarani <99157473+IT21266546@users.noreply.github.com>
2023-09-26 15:34:25 +0530

    feat : header

--- src/Components/Header_footer/header.tsx
+++ src/Components/Header_footer/header.tsx
@@ -1,10 +1,79 @@
-import React from "react";
+import { AppBar, Toolbar, Button } from '@mui/material';
+// import { auth } from '../../firebase';
+// import { signOut } from "firebase/auth";
 
-const Header = () =>{
-    return(
-        <>
-            header
-        </>
+import { Link } from 'react-router-dom';
+// import { CityLogo } from '../Utils/tools';
+// import { showErrorToast, showSuccessToast} from "../Utils/tools";
+// import { logOutHandler } from '../Utils/tools';
+
+
+const Header = ({ user }: any) => {
+
+
+    // const logOutHandler = () =>{
+    //     console.log("in the function")
+    //     signOut(auth).then(() => {
+    //         //alert('signed out');
+    //         showSuccessToast('successfully signed out');
+    //       }).catch((error) => {
+    //         // An error happened.
+    //         showErrorToast(error.message);
+    //         console.log(error)
+    //       });
+    // }
+
+    return (
+        <AppBar
+            position='fixed'
+            style={{
+                backgroundColor: '#98c5e9',
+                boxShadow: 'none',
+                padding: '10px 0',
+                borderBottom: '2px solid #00285e'
+
+            }}
+        >
+            <Toolbar style={{ display: 'flex' }}>
+                <div style={{ flexGrow: 1 }}>
+                    <div className="header_logo">
+                        {/* <CityLogo
+                            link={true}
+                            linkTo={'/'}
+                            width="70px"
+                            height="70px"
+                        /> */}
+                        logo
+                    </div>
+                </div>
+
+                <Link to="/the_team">
+                    <Button color="inherit">The team</Button>
+                </Link>
+                <Link to="/matches">
+                    <Button color="inherit">Matches</Button>
+                </Link>
+
+                {user ?
+                <>
+                    <Link to="/dashboard">
+                        <Button color="inherit">Dashboard</Button>
+                    </Link>
+
+                    
+                    {/* <Button color="inherit"
+                            onClick={logOutHandler}
+                    >
+                        Log out
+                    </Button> */}
+                    
+                </>
+                    : null
+                }
+
+
+            </Toolbar>
+        </AppBar>
     )
 }
 
