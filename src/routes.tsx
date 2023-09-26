8296bb4f51fd0e95c2f9ece76980bf1fb8393d00
Sanduni Sathsarani <99157473+IT21266546@users.noreply.github.com>
2023-09-26 14:31:06 +0530

    feat : routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import { Route, BrowserRouter, Switch } from 'react-router-dom'; 
-import Header from './Header'; 
+import { Route, BrowserRouter, Routes as RouterRoutes } from 'react-router-dom';
+
+import Header from './Components/Header_footer/header';
+import Footer from './Components/Header_footer/footer';
+import Home from './Components/Home';
 
 function Routes() {
   return (
     <BrowserRouter>
       <Header />
-      <Switch>
-        <Route path="/" exact component="" /> 
-      </Switch>
+      <RouterRoutes>
+        <Route path="/" element={<Home />} />
+        {/* Add more routes as needed */}
+      </RouterRoutes>
+      <Footer />
     </BrowserRouter>
   );
 }
 
 export default Routes;
-
