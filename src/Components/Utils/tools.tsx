42bd82388d8a7730b711e5d7fe57365ef756e570
Sanduni Sathsarani <99157473+IT21266546@users.noreply.github.com>
2023-09-26 15:39:42 +0530

    feat: tool

---
+++ src/Components/Utils/tools.tsx
@@ -0,0 +1,85 @@
+import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { auth } from '../../firebase';
+import { signOut } from "firebase/auth";
+
+
+import mcitylogo from '../../Resources/images/logos/manchester_city_logo.png';
+
+type CityLogoProps = {
+    link: boolean;
+    linkTo: string;
+    width: string;
+    height: string;
+};
+
+export const CityLogo = ({ link, linkTo, width, height }: CityLogoProps) => {
+    const template = <div
+        className='img_cover'
+        style={{
+            width: width,
+            height: height,
+            background: `url(${mcitylogo}) no-repeat`
+        }}
+    ></div>
+
+    if (link) {
+        return (
+            <Link className='link_logo' to={linkTo}>
+                {template}
+            </Link>
+        )
+    } else {
+        return template
+    }
+}
+
+export const Tag = (props:any) => {
+    const template =
+        <div
+            style={{
+                background:props.bck?props.bck : '#ffffff',
+                fontSize: props.size? props.size:'15px',
+                color: props.color? props.color: '#000000',
+                padding:'5px 10px',
+                display:'inline-block',
+                fontFamily:'Righteous',
+                ...props.add
+            }}
+        >
+            {props.children}
+        </div>
+    if(props.link){
+        return(
+            <Link to={props.linkto}>
+                {template}
+            </Link>
+        )
+    }else{
+        return template
+    }
+}
+
+export const showErrorToast = (msg: string) => {
+    toast.error(msg, {
+        position: toast.POSITION.TOP_LEFT,
+    });
+}
+
+export const showSuccessToast = (msg: string) => {
+    toast.success(msg, {
+        position: toast.POSITION.TOP_LEFT,
+    });
+}
+
+export const logOutHandler = () => {
+    console.log("in the function")
+    signOut(auth).then(() => {
+        //alert('signed out');
+        showSuccessToast('successfully signed out');
+    }).catch((error) => {
+        // An error happened.
+        showErrorToast(error.message);
+        console.log(error)
+    });
+}
