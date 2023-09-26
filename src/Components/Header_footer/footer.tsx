16a14efc53356847a79919a5e4452c8bcab08df6
Sanduni Sathsarani <99157473+IT21266546@users.noreply.github.com>
2023-09-26 16:29:07 +0530

    feat: footer

--- src/Components/Header_footer/footer.tsx
+++ src/Components/Header_footer/footer.tsx
@@ -1,10 +1,21 @@
-import React from "react";
+import { CityLogo } from '../Utils/tools';
 
-const Footer = () =>{
-    return(
-        <>
-            footer
-        </>
+
+const Footer = () => {
+    return (
+        <footer className='bck_blue'>
+            <div className='footer_logo'>
+                <CityLogo
+                    link={true}
+                    linkTo={'/'}
+                    width="70px"
+                    height="70px"
+                />
+            </div>
+            <div className='footer_descl'>
+                Manchester city 2023 All rights reserved
+            </div>
+        </footer>
     )
 }
 
